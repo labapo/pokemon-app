45007ae8faad453912e160258fd6ab87baefe91b
Lorivie Abapo <lorivieabapo@Lorivies-MacBook-Pro.local>
2022-12-12 14:57:37 -0700

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -2,11 +2,18 @@
 const express = require('express');
 const app = express();
 
+///////Other requires
+const pokemon = require('./models/pokemon');
+
 
 
 ////////////////////////// Routes /////////////////////////////////
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!');
+})
+
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon);
 })
 
 
